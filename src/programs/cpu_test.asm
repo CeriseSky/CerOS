4d7703d9a48d0cd25dc69f526c06028229681b59
Indigo Carmine <cerisesky@proton.me>
2025-07-05 20:32:44 +0100

    Added license notice to cpu tests

--- src/programs/cpu_test.asm
+++ src/programs/cpu_test.asm
@@ -1,3 +1,19 @@
+; CerOS Programs
+; Copyright (C) 2025 Indigo Carmine (CeriseSky)
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ; tests the CPU for the following features:
 ; - 32 bit support
 ; - cpuid instruction support
